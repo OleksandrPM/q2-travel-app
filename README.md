f554c28d579f9437775e1e13a3f94624d5976ef8
Oleksandr Moskaliuk <olekspm@gmail.com>
2024-03-16 16:38:31 +0100

    Have set the project structure, added images and fonts

---
+++ README.md
@@ -0,0 +1,4 @@
+Project`s timing:
+
+- Preparing the project, setting gh-pages - 0.5h;
+- Downloading and optimizing the images, preparing the svg-sprite - 1.5h;
--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,14 @@
+import { resolve } from "path";
+import handlebars from "vite-plugin-handlebars";
+
 export default {
   base: "/q2-travel-app",
+  plugins: [
+    handlebars({
+      partialDirectory: [
+        resolve(__dirname, "./src/partials"),
+        resolve(__dirname, "./src/partials/components"),
+      ],
+    }),
+  ],
 };
