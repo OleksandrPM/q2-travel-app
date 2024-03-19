101e3924a1e1a82b8dd56c16d00db20bedf469e1
Oleksandr Moskaliuk <olekspm@gmail.com>
2024-03-19 20:59:39 +0100

    Added carousel js

--- src/js/hero-gallery-pagination.js
+++ src/js/hero-gallery-pagination.js
@@ -1,11 +1,19 @@
 const galleryEl = document.querySelector(".hero-gallery");
+const imgList = galleryEl.querySelector(".hero-gallery_list");
+const itemEls = document.querySelectorAll(".hero-gallery_item");
 const paginationEl = galleryEl.querySelector(".hero-gallery_pagination");
 const buttonEls = paginationEl.querySelectorAll(".pagination_button");
 const textEl = paginationEl.querySelector(".pagination_text");
 
 const toggleClassName = "pulse";
 
+const slideStep = itemEls[1].offsetLeft;
+let imgListPosition = 0;
+let itemOnStart = 1;
+
 textEl.addEventListener("mouseenter", onMouseEnter);
+
+buttonEls[1].addEventListener("click", onForwardBtnClick);
 
 function onMouseEnter() {
   buttonEls.forEach((el) => {
@@ -23,4 +31,36 @@ function onMouseLeave() {
 
   textEl.addEventListener("mouseenter", onMouseEnter);
   textEl.removeEventListener("mouseleave", onMouseLeave);
+}
+
+function onForwardBtnClick() {
+  imgList.style.transform = `translateX(${imgListPosition - slideStep}px)`;
+  imgListPosition -= slideStep;
+  itemOnStart += 1;
+
+  if (itemOnStart === itemEls.length) {
+    buttonEls[1].disabled = true;
+    buttonEls[1].removeEventListener("click", onForwardBtnClick);
+  }
+
+  if (itemOnStart > 1) {
+    buttonEls[0].disabled = false;
+    buttonEls[0].addEventListener("click", onBackBtnClick);
+  }
+}
+
+function onBackBtnClick() {
+  imgList.style.transform = `translateX(${imgListPosition + slideStep}px)`;
+  imgListPosition += slideStep;
+  itemOnStart -= 1;
+
+  if (itemOnStart === 1) {
+    buttonEls[0].disabled = true;
+    buttonEls[0].removeEventListener("click", onBackBtnClick);
+  }
+
+  if (itemOnStart < itemEls.length) {
+    buttonEls[1].disabled = false;
+    buttonEls[1].addEventListener("click", onForwardBtnClick);
+  }
 }
